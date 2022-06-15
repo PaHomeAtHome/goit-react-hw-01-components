aed8e1b59da0d7b3047198ae03aa5a5ca5014800
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-15 21:54:09 +0300

    profile added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,23 +9,23 @@ export default function Profile({username, tag, location, avatar = 'https://cdn-
       alt="User avatar"
       className="avatar"
     />
-    <p className="name">{username}</p>
-    <p className="tag">@{tag}</p>
-    <p className="location">{location}</p>
+    <p className={styles.username}>{username}</p>
+    <p className={styles.tag}>@{tag}</p>
+    <p className={styles.location}>{location}</p>
   </div>
 
-  <ul className="stats">
+  <ul className={styles.stats}>
     <li>
-      <span className="label">Followers</span>
-      <span className="quantity">{stats.followers}</span>
+      <span className={styles.label}>Followers</span>
+      <span className={styles.quantity}>{stats.followers}</span>
     </li>
     <li>
-      <span className="label">Views</span>
-      <span className="quantity">{stats.views}</span>
+      <span className={styles.label}>Views</span>
+      <span className={styles.quantity}>{stats.views}</span>
     </li>
     <li>
-      <span className="label">Likes</span>
-      <span className="quantity">{stats.likes}</span>
+      <span className={styles.label}>Likes</span>
+      <span className={styles.quantity}>{stats.likes}</span>
     </li>
   </ul>
 </div>   
