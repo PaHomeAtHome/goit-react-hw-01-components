a8aee6d86f3ff474d78cfc40f1ad7597227fa7f5
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-16 00:06:38 +0300

    statistics ready

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,14 +1,18 @@
 import styles from './Statistics.module.css';
-// import PropTypes from 'prop-types';
-const colors = ['#00AAAA', '#770077', '#CC0044', '#00AA00', '#0044BB'];
+import PropTypes from 'prop-types';
+const colors = ['#00AAAA', '#770077', '#CC0044', '#00AA00', '#0044BB']
+
+const makeRandomColor = (colors) => {
+    return Math.floor(Math.random() * colors.length);
+}
 
 export default function Statistics({ title, stats }) {
     return <section className={styles.statistics}>
         {title && (<h2 className={styles.title}>{title}</h2>)}
         <ul className={styles.statList}>
-            {stats.map(({ id, label, percentage }, index) =>
+            {stats.map(({ id, label, percentage }) =>
 
-                <li className={styles.item} key={id} style={{backgroundColor: colors[index]}}>
+                <li className={styles.item} key={id} style={{backgroundColor: colors[makeRandomColor(colors)]}}>
                     <span className={styles.label}>{label}</span>
                     <span className={styles.percentage}>{percentage}%</span>
                 </li>
@@ -17,7 +21,7 @@ export default function Statistics({ title, stats }) {
     </section> 
 }
 
-// Statistics.propTypes = {
-//     username: PropTypes.string.isRequired,
-//     stats: PropTypes.objectOf(PropTypes.number),
-// }
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(PropTypes.object).isRequired
+}
